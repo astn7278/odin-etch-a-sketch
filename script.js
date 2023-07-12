cf1f932cb4a2b544433afa67df1d903185fb2d5f
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-07-11 23:19:59 -0700

    Create event listener to change cell color on hover

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ for (let i = 0; i < 16 * 16; i++) {
     cells.classList.add("cells");
     gridContainer.appendChild(cells);
 }
+//create number of cells in grid
+
+const cells = document.querySelectorAll('.cells')
+
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', function (e) {
+        e.target.style.background = 'lightblue';
+    })
+})
+//change color of cell on hover 
 
