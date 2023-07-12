5c0adc2c01f6cffcacc8bebb8b3a37d8c4305757
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-07-11 22:34:18 -0700

    Create div cells in new container div

---
+++ script.js
@@ -0,0 +1,7 @@
+const gridContainer = document.querySelector(".grid-container");
+
+for (let i = 0; i < 16 * 16; i++) {
+  const cells = document.createElement("div");
+  cells.classList.add("cells");
+  gridContainer.appendChild(cells);
+}
