eb3daa7a8e92c857682f086106ddd1064eecc87f
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-07-11 22:56:29 -0700

    Format 16x16 grid

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
 
 for (let i = 0; i < 16 * 16; i++) {
-  const cells = document.createElement("div");
-  cells.classList.add("cells");
-  gridContainer.appendChild(cells);
+    const cells = document.createElement("div");
+    cells.classList.add("cells");
+    gridContainer.appendChild(cells);
 }
+
