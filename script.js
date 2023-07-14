eaf91229f6ecb6c78caaf75bacc0db6e000cddd3
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-07-13 22:23:37 -0700

    Create function to remove old grid and add new based off user input

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
+//create default number of cells in grid
 const gridContainer = document.querySelector(".grid-container");
-
-for (let i = 0; i < 16 * 16; i++) {
-    const cells = document.createElement("div");
-    cells.classList.add("cells");
-    gridContainer.appendChild(cells);
+let gridSize = 16;
+for (let i = 0; i < gridSize * gridSize; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    gridContainer.appendChild(grid);
 }
-//create number of cells in grid
 
-
-const cells = document.querySelectorAll('.cells')
-
+//change color of cell on hover 
+const colorCells = function colorCells() {
+const cells = document.querySelectorAll('.grid')
 cells.forEach((cell) => {
     cell.addEventListener('mouseover', function (e) {
         e.target.style.background = 'lightblue';
     })
 })
-//change color of cell on hover 
+}
+colorCells ();
 
 
+//prompts for new grid size
 const btn = document.querySelector('button')
+const removeGrid = function removeGrid () {
+    const oldGrid = document.querySelectorAll('.grid');
+    oldGrid.forEach((old) => {
+        old.remove();
+    })
 
+}
+
+//removes old grid, creates new grid, resizes cells based off user input
 btn.addEventListener('click', function newGrid () {
-    newGridSize = prompt("Enter New Grid Size", "16");
-
+    gridSize = prompt("Enter New Grid Size", "16");
+    if (gridSize <=100 && gridSize > 0) {
+        removeGrid();
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            gridContainer.appendChild(grid);
+            let newGrid = (800 / gridSize + "px");
+            grid.style.setProperty("height", newGrid);
+            grid.style.setProperty("width", newGrid);
+        }
+    colorCells()
+    }
+    else if (gridSize == 0)  {
+        alert("No new grid found")
+    }
+    else alert("Input exceeds supported grid size") 
 })
+
+
+
